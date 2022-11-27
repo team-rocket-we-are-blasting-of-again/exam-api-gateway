49976c161975d07e47b2cef00d37c33c637b8131
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-11-27 13:35:29 +0100

    fix: dont override the migration script instead create new one

--- core/src/main/resources/db/migration/V3__ActuatorRoutes.sql
+++ core/src/main/resources/db/migration/V3__ActuatorRoutes.sql
@@ -2,7 +2,7 @@ INSERT INTO gateway_route (request_path, forward_uri)
 VALUES ('/actuator/**', 'INTERNAL');
 
 INSERT INTO route_path (path, method, gateway_route_id)
-VALUES ('/actuator/**', 'BASIC', 2);
+VALUES ('/actuator/**', 'NONE', 2);
 
 INSERT INTO route_path_app_role (route_path_id, app_role_id)
 VALUES (4, 4);
