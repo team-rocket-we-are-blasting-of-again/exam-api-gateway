a152ded29b402f9d084ce2f9f28183dfbc6acd4b
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-11-30 23:52:32 +0100

    ci: ups

--- get_xpath_value.sh
+++ get_xpath_value.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-get_xpath_value() {
-    xml=$1
-    path=$2
-    if [ -f $xml ]; then
-      value=$(xpath -e $path $xml | perl -pe 's/^.+?\>//; s/\<.+?$//;')
-      echo -n $value
-    else
-      echo 'Invalid xml file "$xml"!'
-      exit 1;
-    fi
-}
+xml=$1
+path=$2
+if [ -f $xml ]; then
+  value=$(xpath -e $path $xml | perl -pe 's/^.+?\>//; s/\<.+?$//;')
+  echo -n $value
+else
+  echo 'Invalid xml file "$xml"!'
+  exit 1;
+fi
