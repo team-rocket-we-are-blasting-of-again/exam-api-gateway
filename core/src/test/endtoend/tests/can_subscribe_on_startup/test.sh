6f333ec04dfaae9f5789d12fbb5ceedf9c2ef56d
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-12-01 19:23:38 +0100

    feat: add end to end test checking if we can hit other endpoints

--- core/src/test/endtoend/tests/can_subscribe_on_startup/test.sh
+++ core/src/test/endtoend/tests/can_subscribe_on_startup/test.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-get_all_routes() {
+customer_registration_request() {
     basic_auth=$(echo -n "bob:thebuilder" | base64)
 
     curl --request GET \
-        --url http://localhost:8080/gateway/route \
+        --url http://localhost:8080/customer/api/v1/customers \
         --header "Authorization: Basic $basic_auth" \
         --header 'Content-Type: application/json' \
-        --header 'Accept: application/json'
+        --header 'Accept: application/json' \
+        --data '{"firstName":"Bob","lastName":"The Builder", "email":"bob@thebuilder.dk"}'
 }
 
 sleep 10
 
-get_all_routes | {
-    read -r body
-    echo "response body: $body"
-    case $body in
-    *http://customer:8080*)
+customer_registration_request | {
+    read -r http_code
+    echo "response body: $http_code"
+    case $http_code in
+    *200*)
         exit 0
         ;;
     *)
