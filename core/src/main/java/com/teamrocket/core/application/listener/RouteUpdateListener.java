bd58e553f7451c7f4206874fbe3a9afb39bef8b9
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-12-05 22:07:41 +0100

    fix: make sure that all gateway instances will consume the event

--- core/src/main/java/com/teamrocket/core/application/listener/RouteUpdateListener.java
+++ core/src/main/java/com/teamrocket/core/application/listener/RouteUpdateListener.java
@@ -3,22 +3,21 @@ package com.teamrocket.core.application.listener;
 import com.teamrocket.core.service.RouteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(
-    topics = "routeUpdate",
-    groupId = "gateway"
-)
 @Slf4j
 @RequiredArgsConstructor
 public class RouteUpdateListener {
 
     private final RouteService routeService;
 
-    @KafkaHandler(isDefault = true)
+    @KafkaListener(
+        topics = "routeUpdate",
+        // This will make sure that all gateway instances get the message, and will refresh the routes
+        groupId = "gateway-#{T(java.util.UUID).randomUUID().toString()}"
+    )
     void listen(String message) {
         log.info("updating all routes routes");
         routeService.refreshRoutes();
